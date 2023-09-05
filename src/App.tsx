12ea3582d0c3b8a34ef4a4ea3b3a45aaadf91abb
hyesung99 <4538asd@gmail.com>
2023-09-05 22:49:39 +0900

    ✨ Feat: App.tsx profile route추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Layout from './pages/layout';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
       <Route
         path='/'
         element={<LandingPage />}
+      />
+      <Route
+        path='/profile'
+        element={<Profile />}
       />
     </Routes>
   );
