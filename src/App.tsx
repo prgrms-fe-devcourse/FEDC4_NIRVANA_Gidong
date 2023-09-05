88fb0104085829f9bf5a3b8aa47f50419b70ef98
Park Nayeon <within197@naver.com>
2023-09-05 17:37:29 +0900

    ✨ Feat:	react router 셋팅, 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Layout from './pages/layout';
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route
+          path='/landing'
+          element={<LandingPage />}
+        />
+      </Route>
+      <Route
+        path='/'
+        element={<LandingPage />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
