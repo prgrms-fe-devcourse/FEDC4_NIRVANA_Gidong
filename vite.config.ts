b567b3a5ed3a2493857567e82325689beef39d00
Cheshier <chungup5495@gmail.com>
2023-09-06 02:22:21 +0900

    🚀 Build: vite-tsconfig-paths 라이브러리 추가
    라이브러리 추가 및 vite.config.ts 파일에 plugin 등록

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [
+//       { find: '@', replacement: 'src' },
+//       {
+//         find: '@components',
+//         replacement: 'src/components'
+//       },
+//       { find: '@utils', replacement: '/src/utils' },
+//       { find: '@hooks', replacement: '/src/hooks' },
+//       { find: '@pages', replacement: '/src/pages' },
+//       { find: '@assets', replacement: '/src/assets' },
+//       { find: '@types', replacement: '/src/types' },
+//       {
+//         find: '@constants',
+//         replacement: '/src/constants'
+//       },
+//       { find: '@styles', replacement: '/src/styles' },
+//       {
+//         find: '@colors',
+//         replacement: '/src/styles/colors'
+//       }
+//     ]
+//   }
+// });
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: 'src' },
-      {
-        find: '@components',
-        replacement: 'src/components'
-      },
-      { find: '@utils', replacement: '/src/utils' },
-      { find: '@hooks', replacement: '/src/hooks' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@assets', replacement: '/src/assets' },
-      { find: '@types', replacement: '/src/types' },
-      {
-        find: '@constants',
-        replacement: '/src/constants'
-      },
-      { find: '@styles', replacement: '/src/styles' },
-      {
-        find: '@colors',
-        replacement: '/src/styles/colors'
-      }
-    ]
-  }
+  plugins: [react(), tsconfigPaths()]
 });
