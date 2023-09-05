1037c5ec9faeefeba9c108a99086d5bfde0035da
hyesung99 <4538asd@gmail.com>
2023-09-05 17:17:26 +0900

    ✨ Feat: 타입 별칭 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@utils', replacement: '/src/utils' },
-      { find: '@hooks', replacement: '/src/hooks' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@assets', replacement: '/src/assets' },
-      { find: '@types', replacement: '/src/types' },
-      { find: '@constants', replacement: '/src/constants' },
-      { find: '@styles', replacement: '/src/styles' },
-      { find: '@colors', replacement: '/src/styles/colors' }
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components')
+      },
+      { find: '@utils', replacement: path.resolve(__dirname, '/src/utils') },
+      { find: '@hooks', replacement: path.resolve(__dirname, '/src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, '/src/pages') },
+      { find: '@assets', replacement: path.resolve(__dirname, '/src/assets') },
+      { find: '@types', replacement: path.resolve(__dirname, '/src/types') },
+      {
+        find: '@constants',
+        replacement: path.resolve(__dirname, '/src/constants')
+      },
+      { find: '@styles', replacement: path.resolve(__dirname, '/src/styles') },
+      {
+        find: '@colors',
+        replacement: path.resolve(__dirname, '/src/styles/colors')
+      }
     ]
   }
 });
