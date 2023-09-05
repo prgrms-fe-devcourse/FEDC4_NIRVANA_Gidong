a1010d8a2fe8a92005713f50a0b5a2f102d47c4b
Park Nayeon <within197@naver.com>
2023-09-06 01:59:56 +0900

    ✨ Feat:	렌딩페이지 추가

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,5 +1,71 @@
+import styled from '@emotion/styled';
+import Link from '../../components/Link';
+import Button from '../../components/Button';
+import logo from '../../assets/logo.svg';
+
+const LandingMain = styled.main`
+  background: ${({ theme }) => theme['linearGradientPurple']};
+  width: 390px;
+  height: 844px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  width: 223px;
+  height: 44px;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  margin-top: 120px;
+  margin-bottom: 534px;
+`;
+
+const ButtonLayout = styled.div`
+  & > a {
+    display: block;
+  }
+  & > a + a {
+    margin-top: 16px;
+  }
+`;
+
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <LandingMain>
+      <Heading />
+      <ButtonLayout>
+        <Link
+          pageLink={'/login'}
+          size={'16'}
+          color={'white'}>
+          <Button
+            width={300}
+            height={50}
+            label={'로그인'}
+            handleClick={() => {}}
+            dark={false}
+            bold={false}
+          />
+        </Link>
+        <Link
+          pageLink={'/signup'}
+          size={'16'}
+          color={'white'}>
+          <Button
+            width={300}
+            height={50}
+            label={'회원가입'}
+            handleClick={() => {}}
+            dark={true}
+            bold={false}
+          />
+        </Link>
+      </ButtonLayout>
+    </LandingMain>
+  );
 };
 
 export default LandingPage;
