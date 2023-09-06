3224b6e1235d9d2d48c04cb4c83c2e22d63d02aa
hyesung99 <4538asd@gmail.com>
2023-09-06 16:48:44 +0900

    ✨ Feat: signUpUser api 추가

--- src/apis/user/getUserData.ts
+++ src/apis/user/getUserData.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
+import { User } from '../../types/User';
 import { API_BASE_URL } from '../../constants/Api';
+
 const getUserData = async (userId: string) => {
   const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
   return response;
 };
 
-export { getUserData };
+const signUpUser = async (userData: {
+  email: string;
+  password: string;
+  fullName: string;
+}) => {
+  const response = await axios.post(`${API_BASE_URL}/signup`, userData);
+  return response;
+};
+
+export { getUserData, signUpUser };
