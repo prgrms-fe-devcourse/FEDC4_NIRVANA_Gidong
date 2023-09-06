b161157dae5102f9d6a9e4eb58ff5b4f19177e97
hyesung99 <4538asd@gmail.com>
2023-09-06 16:27:00 +0900

    ✨ Feat: react-query설정, getUserData api추가

---
+++ src/apis/user/getUserData.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../../constants/Api';
+const getUserData = async (userId: string) => {
+  const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
+  return response;
+};
+
+export { getUserData };
