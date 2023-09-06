d5d12b49c77f1c6d942a9eaf880989ed21ef99db
Park Nayeon <within197@naver.com>
2023-09-06 14:15:59 +0900

    ✨ Feat:	미리보기 링크 추가

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import Link from '../../components/Link';
 import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
+import Icon from '@/components/Icon';
 
 const LandingMain = styled.main`
   background: ${({ theme }) => theme['linearGradientPurple']};
@@ -20,10 +21,10 @@ const Heading = styled.h1`
   background-size: contain;
   background-position: center;
   margin-top: 120px;
-  margin-bottom: 534px;
+  margin-bottom: 469px;
 `;
 
-const ButtonLayout = styled.div`
+const LinkLayout = styled.div`
   & > a {
     display: block;
   }
@@ -32,38 +33,79 @@ const ButtonLayout = styled.div`
   }
 `;
 
+const PreviewLink = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 26px;
+  & > span {
+    color: ${({ theme }) => theme.white};
+    margin-bottom: 5px;
+  }
+
+  & > a {
+    display: block;
+    padding: 5px;
+    border-bottom: 1px solid ${({ theme }) => theme.white};
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+  }
+
+  & > a > span {
+    margin-left: 5px;
+  }
+`;
+
+const PreviewSpan = styled.span`
+  font-size: 12px;
+`;
+
 const LandingPage = () => {
   return (
     <LandingMain>
       <Heading />
-      <ButtonLayout>
+      <PreviewLink>
+        <PreviewSpan>다른 사람들의 후기를 먼저 만나보세요.</PreviewSpan>
         <Link
-          pageLink={'/login'}
-          size={'16'}
-          color={'white'}>
+          pageLink='/posts'
+          size='12'
+          color='white'>
+          명상 후기 보러가기
+          <Icon
+            name='arrow_forward'
+            size={11}
+            color='white'></Icon>
+        </Link>
+      </PreviewLink>
+      <LinkLayout>
+        <Link
+          pageLink='/login'
+          size='16'
+          color='white'>
           <Button
             width={300}
             height={50}
-            label={'로그인'}
+            label='로그인'
             handleClick={() => {}}
             dark={false}
             bold={false}
           />
         </Link>
         <Link
-          pageLink={'/signup'}
-          size={'16'}
-          color={'white'}>
+          pageLink='/signup'
+          size='16'
+          color='white'>
           <Button
             width={300}
             height={50}
-            label={'회원가입'}
+            label='회원가입'
             handleClick={() => {}}
             dark={true}
             bold={false}
           />
         </Link>
-      </ButtonLayout>
+      </LinkLayout>
     </LandingMain>
   );
 };
