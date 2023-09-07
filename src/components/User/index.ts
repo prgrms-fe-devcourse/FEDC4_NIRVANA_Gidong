99571d2c76d4d677453e59a967468b236bc2a64c
Park Nayeon <within197@naver.com>
2023-09-07 15:25:45 +0900

    ✨ Feat:	UserText 컴포넌트 추기

--- src/components/User/index.ts
+++
@@ -1,3 +0,0 @@
-import FollowUser from './FollowUser';
-
-export { FollowUser };
---
+++ src/components/UserText/UserText.style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const UserIdSpan = styled.span`
+  font-size: 16px;
+  color: ${({ theme }) => theme.greyLight};
+`;
+
+export const UserNameSpan = styled.span`
+  font-size: 16px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.black};
+  margin-left: 15px;
+  margin-right: 2px;
+`;
