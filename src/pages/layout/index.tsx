b8a5f4bf7f6b288df3cc93a540c72687429a7ba4
Park Nayeon <within197@naver.com>
2023-09-07 16:25:41 +0900

    🚨 Fix: Header - header 스타일, 컴포넌트 분리

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import Header from '@/components/header';
+import { Header } from '@/components/Header';
 import { LayoutContainer } from './styles';
 
 const Layout = () => {
