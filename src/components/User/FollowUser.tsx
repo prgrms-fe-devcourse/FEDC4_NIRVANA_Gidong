862b0743c0e3c1ebb0eed282691325e6e5cdd604
hyesung99 <4538asd@gmail.com>
2023-09-07 13:53:33 +0900

    ✨ Feat: FollowUser 컴포넌트 스타일 분리

--- src/components/User/FollowUser.tsx
+++ src/components/User/FollowUser.tsx
@@ -1,22 +1,5 @@
-import styled from '@emotion/styled';
 import { Avatar } from '../Avatar';
-
-const UserItem = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const UserNickName = styled.span`
-  font-size: 16px;
-  font-weight: 700;
-  color: ${({ theme }) => theme.black};
-  margin-left: 15px;
-  margin-right: 2px;
-`;
-const UserId = styled.span`
-  font-size: 16px;
-  color: ${({ theme }) => theme.greyLight};
-`;
+import { UserItem, UserNickName, UserId } from './FollowUser.style';
 
 interface FollowUserProps {
   alt: string;
