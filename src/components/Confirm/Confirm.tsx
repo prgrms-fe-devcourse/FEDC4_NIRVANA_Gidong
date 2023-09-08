8e7abaa2ef7a71080ad04d2e8f2fb6a6a4ef6e21
Suyeon <msjgr2801@gmail.com>
2023-09-08 14:20:03 +0900

    ✨ Feat: Confirm 컴포넌트 로직과 스타일 분리

---
+++ src/components/Confirm/Confirm.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Button from '../Button';
+import Link from '../Link';
+import {
+  DeemBackground,
+  ConfirmBackground,
+  IconWrapper,
+  ContentWrapper,
+  NavButtonWrapper
+} from './Confirm.style';
+
+interface AlertProps {
+  width: number;
+  height: number;
+  emoji: string;
+  emojiSize: number;
+  content: string;
+  contentFontSize: number;
+  nextPageLink: string;
+}
+
+const Alert = ({
+  width = 330,
+  height = 390,
+  emoji,
+  emojiSize = 4.5,
+  content,
+  contentFontSize = 1,
+  nextPageLink
+}: Partial<AlertProps>) => {
+  const [disabled, setDisabled] = useState(false);
+  return (
+    <DeemBackground disabled={disabled}>
+      <ConfirmBackground
+        width={width}
+        height={height}>
+        <IconWrapper emojiSize={emojiSize}>{emoji}</IconWrapper>
+        <ContentWrapper contentFontSize={contentFontSize}>
+          {content}
+          <NavButtonWrapper>
+            <Button
+              handleClick={() => setDisabled(true)}
+              width={120}
+              height={50}
+              dark={false}
+              bold={false}
+              label={'취소'}></Button>
+            <Link pageLink={nextPageLink}>
+              <Button
+                width={120}
+                height={50}
+                dark={true}
+                bold={true}
+                label={'확인'}
+              />
+            </Link>
+          </NavButtonWrapper>
+        </ContentWrapper>
+      </ConfirmBackground>
+    </DeemBackground>
+  );
+};
+export default Alert;
