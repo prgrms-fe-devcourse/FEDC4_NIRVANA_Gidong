caba9e05d0545d516e45094a544451ca4b7611d9
Park Nayeon <within197@naver.com>
2023-09-08 18:31:08 +0900

    ✨ Feat:	FollowUser 컴포넌트 추가

---
+++ src/pages/profile/components/FollowUser/FollowUser.style.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const FollowUserLayout = styled.div`
+  display: flex;
+  align-items: center;
+  & > div {
+    margin-right: 15px;
+  }
+
+  & > span:last-child {
+    margin-left: 2px;
+  }
+
+  & .dot {
+    bottom: 1px;
+    right: 1px;
+  }
+`;
---
+++ src/pages/profile/components/FollowUser/FollowUser.tsx
@@ -0,0 +1,33 @@
+import { FollowUserLayout } from './FollowUser.style';
+import { UserId, UserName } from '@/components/UserText';
+import { BadgeAvatar } from '@/components/Avatar';
+
+interface UserData {
+  image: string;
+  fullName: string;
+  isOnline: boolean;
+  email: string;
+}
+
+interface FollowUserProps {
+  userData: UserData;
+}
+
+const FollowUser = ({ userData }: FollowUserProps) => {
+  const { fullName, email, isOnline, image } = userData;
+
+  return (
+    <FollowUserLayout>
+      <BadgeAvatar
+        alt={fullName}
+        src={image}
+        size={39}
+        online={isOnline}
+      />
+      <UserId email={email} />
+      <UserName>{fullName}</UserName>
+    </FollowUserLayout>
+  );
+};
+
+export default FollowUser;
