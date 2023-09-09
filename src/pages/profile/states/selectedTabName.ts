91705c4b28c95bfd1c7610c252230fd363365150
hyesung99 <4538asd@gmail.com>
2023-09-09 22:55:31 +0900

    ✨ Feat: atom index를 전역관리

--- src/pages/profile/states/selectedTabName.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import { PROFILE_TABS } from '../constants/profileTabs';
-
-export const selectedTabNameState = atom({
-  key: 'selectedTabName',
-  default: PROFILE_TABS.MEDITATION
-});
