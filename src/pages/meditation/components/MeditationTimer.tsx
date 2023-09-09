0c880979b4c1b8d7d37bd4a733899ef9c6157ce5
Cheshier <chungup5495@gmail.com>
2023-09-10 03:36:40 +0900

    ✨ Feat: 타이머 시작 및 남은 시간 표시 기능 추가

--- src/pages/meditation/components/MeditationTimer.tsx
+++ src/pages/meditation/components/MeditationTimer.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
+import { atom, useRecoilState } from 'recoil';
 import { useState } from 'react';
+import formatTime from '@utils/formatTime';
 
 const TimerContainer = styled.div`
   display: flex;
@@ -26,13 +28,36 @@ const TimerElement = styled.button`
   color: ${({ theme }) => theme['white']};
   font-size: 1.5rem;
   font-weight: bold;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
+export const meditationTime = atom({
+  key: 'meditationTime',
+  default: 0
+});
+
 const MeditationTimer = () => {
-  const [time, setTime] = useState('START');
+  const [timeLabel, setTimeLabel] = useState('START');
+  const [time, setTime] = useRecoilState(meditationTime);
+
+  const startTimer = () => {
+    const timer = setInterval(() => {
+      if (time > 0) {
+        setTime((prevTime) => prevTime - 1);
+      } else {
+        clearInterval(timer);
+        setTimeLabel('END!');
+        document.dispatchEvent(new Event('END-MEDITATION'));
+      }
+    }, 1000);
+    document.dispatchEvent(new Event('START-MEDITATION'));
+  };
+
   return (
     <TimerContainer>
-      <TimerElement>{time}</TimerElement>
+      <TimerElement onClick={startTimer}>{formatTime(time)}</TimerElement>
     </TimerContainer>
   );
 };
