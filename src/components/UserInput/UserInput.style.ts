a0a34433d9e35bf2d2a6e1880584932629cfe839
Suyeon <msjgr2801@gmail.com>
2023-09-11 14:21:37 +0900

    🔨 Refactoring: UserInput 컴포넌트에서 스타일 분리

---
+++ src/components/UserInput/UserInput.style.ts
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+export const SignUpLabel = styled.label`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  margin-left: 10px;
+  margin-bottom: 10px;
+`;
+
+export const SignUpInput = styled.input`
+  box-sizing: border-box;
+  width: 300px;
+  height: 45px;
+  padding-left: 10px;
+  border: 0.5px solid #7e7e7e;
+  border-radius: 10px;
+`;
+
+export const InputContainer = styled.div`
+  margin: 10px 0;
+`;
+
+export const SignUpError = styled.span`
+  font-size: 10px;
+  font-weight: 400;
+  color: ${({ theme }) => theme['redVivid']};
+  margin-left: 10px;
+`;
+
+export const SignUpSuccess = styled.span`
+  font-size: 10px;
+  font-weight: 400;
+  color: ${({ theme }) => theme['greenVivid']};
+  margin-left: 10px;
+`;
