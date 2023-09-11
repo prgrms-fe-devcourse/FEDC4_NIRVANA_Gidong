af00b8c8842ff6763775a3f4ff9d12da5e40cd84
hyesung99 <4538asd@gmail.com>
2023-09-11 18:02:50 +0900

    🚨 Fix: provider color 오류 수정

--- src/pages/profile/components/ProfileTabs/ProfileTabItem.style.ts
+++ src/pages/profile/components/ProfileTabs/ProfileTabItem.style.ts
@@ -8,9 +8,9 @@ export const ProfileTabItemContainer = styled.div<ProfileTabsItemProps>`
   height: 50px;
   border-bottom: 3px solid
     ${({ selected, theme }) =>
-      selected ? theme['purpleVivid'] : 'transparent'};
+      selected ? theme.color.purpleVivid : 'transparent'};
   color: ${({ selected, theme }) =>
-    selected ? theme['black'] : theme['greyLight']};
+    selected ? theme.color.black : theme.color.greyLight};
   display: flex;
   align-items: center;
   justify-content: center;
