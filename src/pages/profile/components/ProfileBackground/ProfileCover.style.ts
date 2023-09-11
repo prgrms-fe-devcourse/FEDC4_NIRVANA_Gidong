b9661fefe94cb20411692588c52c2b0bee436bb7
hyesung99 <4538asd@gmail.com>
2023-09-11 21:45:14 +0900

    ✨ Feat: 커버 이미지 기본값 설정

--- src/pages/profile/components/ProfileBackground/ProfileCover.style.ts
+++ src/pages/profile/components/ProfileBackground/ProfileCover.style.ts
@@ -5,5 +5,8 @@ export const ProfileCoverImage = styled.img`
   object-fit: cover;
   width: 100%;
   height: fit-content;
+  min-height: 100px;
   top: 0;
+  background-image: ${({ theme, src }) =>
+    src ?? theme.color.linearGradientPurple};
 `;
