833be9374a72829992b7d61dd6fd418b65440b3a
Park Nayeon <within197@naver.com>
2023-09-12 12:15:00 +0900

    ✨ Feat:	follow,unfollow 네트워크 요청 함수 추가

---
+++ src/apis/follow/index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../../constants/Api';
+
+const postFollowUser = async (userId: string) => {
+  const response = await axios.post(
+    `${API_BASE_URL}/follow/create`,
+    { userId },
+    {
+      headers: {
+        // test token
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZmYxNmNjMTY5Yzc5MDU3YjVmOGVjMCIsImVtYWlsIjoibmFuYTEyNEBuYXZlci5jb20ifSwiaWF0IjoxNjk0NDM5MzE1fQ.heYeAayvX78n0NooS-7H4HlbeHCvquXdFl7tRIVkEHM'
+      }
+    }
+  );
+  return response.data;
+};
+
+const deleteUnfollowUser = async (userId: string) => {
+  const response = await axios.delete(`${API_BASE_URL}/follow/delete`, {
+    data: { id: userId },
+    headers: {
+      // test token
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZmYxNmNjMTY5Yzc5MDU3YjVmOGVjMCIsImVtYWlsIjoibmFuYTEyNEBuYXZlci5jb20ifSwiaWF0IjoxNjk0NDM5MzE1fQ.heYeAayvX78n0NooS-7H4HlbeHCvquXdFl7tRIVkEHM'
+    }
+  });
+  return response.data;
+};
+
+export { postFollowUser, deleteUnfollowUser };
