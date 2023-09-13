508901a316fc84a16e907383918829d54baf9ff0
Suyeon <msjgr2801@gmail.com>
2023-09-13 17:31:41 +0900

    ✨ Feat: API try-catch 구문 적용

--- src/apis/posting/index.ts
+++ src/apis/posting/index.ts
@@ -2,12 +2,20 @@ import axios from 'axios';
 import { API_BASE_URL } from '@/constants/Api';
 
 const postCreateNewPost = async (token: string, formData: FormData) => {
-  const response = await axios.post(`${API_BASE_URL}/posts/create`, formData, {
-    headers: {
-      Authorization: token
-    }
-  });
-  return response;
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/posts/create`,
+      formData,
+      {
+        headers: {
+          Authorization: token
+        }
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default postCreateNewPost;
