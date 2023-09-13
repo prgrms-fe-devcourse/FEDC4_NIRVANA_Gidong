fa3f4c7c76f1bc20c1d3c65f353ffb7aa40b9a7d
hyesung99 <4538asd@gmail.com>
2023-09-13 16:54:13 +0900

    ✨Feat : 아바타,커버 변경 api추가

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@/constants/Api';
 
-const getUser = async (userId: string) => {
-  const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
-  return response.data;
-};
-
 interface PutUpdateUserParams {
   username?: string;
   fullName?: string;
   token: string;
 }
+
+const getUser = async (userId: string) => {
+  const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
+  return response.data;
+};
+
 const putUpdateUser = async ({
   username,
   fullName,
@@ -31,4 +32,36 @@ const putUpdateUser = async ({
   return response.data;
 };
 
-export { getUser, putUpdateUser };
+const postUploadAvatar = async (image: string, token: string) => {
+  const formData = new FormData();
+  formData.append('image', image);
+  formData.append('isCover', 'true');
+  const response = await axios.post(
+    `${API_BASE_URL}/users/upload-photo`,
+    formData,
+    {
+      headers: {
+        Authorization: token
+      }
+    }
+  );
+  return response.data;
+};
+
+const postUploadCover = async (image: string, token: string) => {
+  const formData = new FormData();
+  formData.append('image', image);
+  formData.append('isCover', 'true');
+  const response = await axios.post(
+    `${API_BASE_URL}/users/upload-photo`,
+    formData,
+    {
+      headers: {
+        Authorization: token
+      }
+    }
+  );
+  return response.data;
+};
+
+export { getUser, putUpdateUser, postUploadAvatar, postUploadCover };
