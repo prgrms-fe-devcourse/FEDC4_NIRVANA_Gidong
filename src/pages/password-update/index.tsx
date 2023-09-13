5d011094beba2ab9ca102769d3f863133b528f31
김민재 <kimminje7267@naver.com>
2023-09-13 18:00:17 +0900

    ✨Feat: 비밀번호 재설정 성공시 Alert창 출력 추가
    Alert창에서 확인버튼 누르면 '/login' 페이지로 이동

--- src/pages/password-update/index.tsx
+++ src/pages/password-update/index.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { useNavigate } from 'react-router-dom';
+// import { useRecoilValue } from 'recoil';
 import putUpdatePassword from '@apis/password';
 import { Alert } from '@components/Alert';
 import { Footer } from '@components/Footer';
 import { Button } from '@components/Button';
 import { UserInput } from '@components/UserInput';
-import { userState } from '@/states/userState';
+// import { userState } from '@/states/userState';
 import { USER_INPUT, LABEL, PASSWORD_HINT } from './constants';
 import isPasswordOk from './validations';
 import { PasswordHint } from './components';
@@ -19,11 +18,13 @@ import {
 } from './styles';
 
 const PasswordUpdate = () => {
-  const { token } = useRecoilValue(userState);
-  const customToken = `bearer ${token}`;
-  const navigate = useNavigate();
+  // const { token } = useRecoilValue(userState);
+  // const customToken = `bearer ${token}`;
+  const customToken =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZmYxNmNjMTY5Yzc5MDU3YjVmOGVjMCIsImVtYWlsIjoibmFuYTEyNEBuYXZlci5jb20ifSwiaWF0IjoxNjk0NDM5MzE1fQ.heYeAayvX78n0NooS-7H4HlbeHCvquXdFl7tRIVkEHM';
   const [password, setPassword] = useState<string>('');
   const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+  const [passwordChanged, setPasswordChanged] = useState<boolean>(false);
   let timer = 0;
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -50,9 +51,9 @@ const PasswordUpdate = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (password && passwordConfirm && password === passwordConfirm) {
-      putUpdatePassword({ password, token: customToken }).then(() =>
-        navigate('/password-complete')
-      );
+      putUpdatePassword({ password, token: customToken })
+        .then(() => setPasswordChanged(true))
+        .catch((error) => console.log(error));
     }
   };
 
@@ -63,12 +64,14 @@ const PasswordUpdate = () => {
           <Logo />
         </LogoContainer>
 
-        <Alert
-          emoji={'✅'}
-          content={'비밀번호가 정상적으로 변경되었습니다.'}
-          buttonLabel={'확인'}
-          nextPageLink={'/login'}
-        />
+        {passwordChanged && (
+          <Alert
+            emoji={'✅'}
+            content={'비밀번호가 정상적으로 변경되었습니다.'}
+            buttonLabel={'확인'}
+            nextPageLink={'/login'}
+          />
+        )}
 
         <PasswordUpdateForm onSubmit={handleSubmit}>
           <PasswordHint text={PASSWORD_HINT} />
