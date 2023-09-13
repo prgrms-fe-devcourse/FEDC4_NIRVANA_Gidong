634d664cf5183f74bda4b8891b0cd23647d55e75
Suyeon <msjgr2801@gmail.com>
2023-09-13 13:44:38 +0900

    ✨ Feat: post/Posting formData 생성 util 함수 구현

---
+++ src/pages/posting/utils/makeFormData.ts
@@ -0,0 +1,11 @@
+const makeFormData = (content: string, channelId: string) => {
+  const formData = new FormData();
+
+  formData.append('title', content);
+  formData.append('channelId', channelId);
+  formData.append('image', null);
+
+  return formData;
+}
+
+export default makeFormData;
