70de6e956ad88afb2098e07671b60d486d03f3e2
Cheshier <chungup5495@gmail.com>
2023-09-14 17:09:46 +0900

    ✨ Feat: ThemePicker Item 스크롤 기능 추가
    Thanks to 수연갓

--- src/pages/meditation/components/MeditationThemePicker/MeditationThemePicker.style.ts
+++ src/pages/meditation/components/MeditationThemePicker/MeditationThemePicker.style.ts
@@ -3,6 +3,20 @@ import styled from '@emotion/styled';
 const ThemePickerContainer = styled.div`
   display: flex;
   margin-top: 20px;
-`;
+  overflow: hidden;
+  max-width: 500px;
+  width: 80%;
+  box-sizing: content-box;
+  justify-content: flex-start;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
+  & > button {
+    margin-left: 5px;
+    margin-right: 5px;
+    flex-shrink: 0;
+  }
+`;
 export { ThemePickerContainer };
