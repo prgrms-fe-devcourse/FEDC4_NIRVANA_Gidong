8615fa7cd9f892f202077664220ccd89940f2c05
hyesung99 <4538asd@gmail.com>
2023-09-15 10:03:02 +0900

    ✨Feat : Footer Link 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,24 @@
 import { Icon } from '../Icon';
 import { Link } from '../Link';
 import { StyledFooter } from './Footer.style';
-
-const iconInfos = [
-  { name: 'home', size: 35, link: '/posts' },
-  { name: 'self_improvement', size: 35, link: '/meditation' },
-  { name: 'person', size: 35, link: '/profile:userId' }
-];
+import useSessionStorage from '@hooks/useSessionStorage';
+import { User } from '@/types/User';
 
 export const Footer = () => {
+  const [userData] = useSessionStorage<Pick<User, '_id' | 'token'>>(
+    'userData',
+    {
+      _id: '',
+      token: ''
+    }
+  );
+
+  const iconInfos = [
+    { name: 'home', size: 35, link: '/posts' },
+    { name: 'self_improvement', size: 35, link: '/meditation' },
+    { name: 'person', size: 35, link: `/profile/${userData._id}` }
+  ];
+
   return (
     <StyledFooter>
       {iconInfos.map((icon) => (
