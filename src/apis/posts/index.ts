fd1865d08cd98974cf352016e778bd5360005d94
Suyeon <msjgr2801@gmail.com>
2023-09-18 15:52:16 +0900

    ✨ Feat: posts 데이터 가져와서 출력하기

--- src/apis/posts/index.ts
+++ src/apis/posts/index.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@/constants/Api';
 
-export const getPosts = async(channelId: string) => {
+export const getPosts = async (channelId: string) => {
   try {
-    const url = `${API_BASE_URL}/posts/channel/${channelId}`
+    const url = `${API_BASE_URL}/posts/channel/${channelId}`;
+    console.log(url);
     const response = axios.get(url);
 
     return response;
   } catch (error) {
     console.log(error);
   }
-}
+};
--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,21 +1,24 @@
 import { useState, useEffect, useCallback } from 'react';
 
-import { ThemePicker } from '@components/ThemePicker';
-import { meditationChannelInfo } from '@pages/meditation/models/channelInfo';
+import { StyledPostsPage } from './Posts.style';
+import { PostPreview } from '@components/PostPreview';
+import { getPosts } from '@apis/posts';
+import type { Post } from '@types';
+
+// import { ThemePicker } from '@components/ThemePicker';
+// import { meditationChannelInfo } from '@pages/meditation/models/channelInfo';
 
 const Posts = () => {
-  // 1. themePicker 컴포넌트에 쓰일 데이터 
-  const themeInfo = new Map(meditationChannelInfo);
+  // 1. themePicker 컴포넌트에 쓰일 데이터
+  // const themeInfo = new Map(meditationChannelInfo);
   // 2. 출력할 포스트 데이터들
-  const [postsData, setPostsData] = useState([]);
+  const [postsData, setPostsData] = useState<Post[]>([]);
 
   // 3. 포스트 가져오는 함수
   const fetchPosts = useCallback(async () => {
-    const data = await getPosts('').then(() => {
-      setPostsData(data);
-    }) 
-
-
+    const data = await getPosts('65003530a72a0d2e63f12878');
+    // console.log(data.data);
+    setPostsData(data.data);
   }, []);
 
   // 4. useEffect 시 API 로 postsData 상태 변경하기
@@ -25,10 +28,16 @@ const Posts = () => {
 
   return (
     // 5. 렌더링 - themePicker + PostPreview + 무한 스크롤
-    <StylePostsPage>
-      <ThemePicker themeInfo={themeInfo} />
-      { postsData.map((post) => <PostPreview />)}
-    </StylePostsPage>
+    <StyledPostsPage>
+      {postsData.map((post: Post) => (
+        <PostPreview
+          post={post}
+          totalLikes={post.likes.length}
+          totalComments={post.comments.length}
+          noneProfile={false}
+        />
+      ))}
+    </StyledPostsPage>
   );
 };
 
