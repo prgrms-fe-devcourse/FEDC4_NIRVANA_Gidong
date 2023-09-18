a57f677651b57f2715a6b74de71cf53478c02979
Suyeon <msjgr2801@gmail.com>
2023-09-18 15:20:25 +0900

    🔨 Refactoring: 안쓰는 라이브러리 삭제

--- src/components/ThemePicker/hooks/useScrollButton.ts
+++ src/components/ThemePicker/hooks/useScrollButton.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 const useScrollButton = () => {
   const scrollRef = useRef(null);
