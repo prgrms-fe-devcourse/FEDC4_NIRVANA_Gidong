79993e44331965a29812545fa2dfceaf2e25904d
hyesung99 <4538asd@gmail.com>
2023-09-18 16:50:53 +0900

    💄 Style:	사이드바 위치 modal-root로 변경 => 불필요한 스타일 제거

--- src/pages/profile/components/setting/SettingSideBar.style.ts
+++ src/pages/profile/components/setting/SettingSideBar.style.ts
@@ -1,33 +1,23 @@
 import styled from '@emotion/styled';
 
-interface SettingSideBarProps {
-  active: boolean;
-}
-
-export const SettingSideBarSection = styled.section<SettingSideBarProps>`
+export const SettingSideBarSection = styled.section`
   display: flex;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: 2;
-  transform: ${({ active }) =>
-    active ? 'translateX(0%)' : 'translateX(200%)'};
+  width: 100vw;
+  height: 100vh;
+  max-width: 768px;
 `;
 
 export const SettingSideBarBackground = styled.div`
   width: 100%;
+  height: 100%;
   background-color: ${({ theme }) => theme.color.transparentGreyBackground};
   flex: 1;
 `;
 
-export const SettingSideBarPage = styled.div<SettingSideBarProps>`
+export const SettingRightSideBar = styled.div`
   padding: 43px 20px 0;
-  right: 0;
-  position: relative;
-  pointer-events: none;
   width: 100%;
   height: 100%;
-  float: right;
   background-color: ${({ theme }) => theme.color.white};
   flex: 5;
 `;
@@ -39,7 +29,9 @@ export const Heading = styled.h2`
   margin-bottom: 24px;
 `;
 
-export const SettingUl = styled.ul``;
+export const SettingUl = styled.ul`
+  z-index: 1;
+`;
 
 export const SettingLi = styled.li`
   ${({ theme }) => theme.style.flexAlignCenter}
@@ -48,5 +40,4 @@ export const SettingLi = styled.li`
   font-size: 16px;
   color: ${({ theme }) => theme.color.black};
   border-bottom: 0.5px solid ${({ theme }) => theme.color.greyLight};
-  cursor: pointer;
 `;
