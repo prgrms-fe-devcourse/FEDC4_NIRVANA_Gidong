01448cb13ec52c0288b665225b849b1927e3800b
hyesung99 <4538asd@gmail.com>
2023-09-18 16:49:50 +0900

    ✨Feat : useSessionStorage removeValue 추가

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 function useSessionStorage<T>(
   key: string,
   initialValue: T
-): [T, React.Dispatch<React.SetStateAction<T>>] {
+): [T, React.Dispatch<React.SetStateAction<T>>, () => void] {
   const storedValue = sessionStorage.getItem(key);
   const initial = storedValue ? JSON.parse(storedValue) : initialValue;
   const [value, setValue] = useState<T>(initial);
@@ -12,7 +12,11 @@ function useSessionStorage<T>(
     sessionStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
-  return [value, setValue];
+  const removeValue = () => {
+    sessionStorage.removeItem(key);
+  };
+
+  return [value, setValue, removeValue];
 }
 
 export default useSessionStorage;
