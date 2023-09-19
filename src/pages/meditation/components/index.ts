2fdff197672c75d80b7e16253383e7638993f334
Cheshier <chungup5495@gmail.com>
2023-09-19 18:21:12 +0900

    🗑️ Remove: 사용하지 않는 MeditationThemePicker 제거

--- src/pages/meditation/components/index.ts
+++ src/pages/meditation/components/index.ts
@@ -1,13 +1,11 @@
 import MeditationEndButton from './MeditationEndButton';
 import MeditationLabel from './MeditationLabel';
-import MeditationThemePicker from './MeditationThemePicker';
 import MeditationTimer from './MeditationTimer';
 import MeditationTimeSetter from './MeditationTimeSetter';
 
 export {
   MeditationEndButton,
   MeditationLabel,
-  MeditationThemePicker,
   MeditationTimer,
   MeditationTimeSetter
 };
