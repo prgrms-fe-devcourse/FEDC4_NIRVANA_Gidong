e5a313a266ba892a3ad40e21a245bfa57d81bd93
hyesung99 <4538asd@gmail.com>
2023-09-19 18:24:21 +0900

    💄 Style:	Icon에 fill 속성 추가

--- src/components/Icon/Icon.style.ts
+++ src/components/Icon/Icon.style.ts
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled';
 
-export const StyledIcon = styled.span<{ size: number; color: string }>`
+export const StyledIcon = styled.span<{
+  size: number;
+  color: string;
+  fill?: boolean;
+}>`
   color: ${({ theme, color }) => theme.color[color]};
   font-size: ${({ size }) => size}px;
+  font-variation-settings: ${({ fill }) => (fill ? `'FILL' 1` : `'FILL' 0`)};
 `;
