112944437008371dcac74ecb72038c0329d5ae16
Cheshier <chungup5495@gmail.com>
2023-09-20 18:22:31 +0900

    🔨 Refactor: posting api 간결화

--- src/apis/posting/index.ts
+++ src/apis/posting/index.ts
@@ -3,16 +3,11 @@ import { API_BASE_URL } from '@/constants/Api';
 
 const postCreateNewPost = async (token: string, formData: FormData) => {
   try {
-    const response = await axios.post(
-      `${API_BASE_URL}/posts/create`,
-      formData,
-      {
-        headers: {
-          Authorization: token
-        }
+    return await axios.post(`${API_BASE_URL}/posts/create`, formData, {
+      headers: {
+        Authorization: token
       }
-    );
-    return response;
+    });
   } catch (error) {
     console.error(error);
   }
