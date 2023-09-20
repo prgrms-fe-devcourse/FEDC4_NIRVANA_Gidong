f80343a491b10cb44886adeac7ad62401e965b12
Cheshier <chungup5495@gmail.com>
2023-09-20 13:05:11 +0900

    💄 Style: 푸터 하부 여백 해결 외 2건
    - Channel Label에 밑줄 표시
    - Prettier 적용

--- src/pages/posting/Posting.tsx
+++ src/pages/posting/Posting.tsx
@@ -2,11 +2,10 @@ import { useLocation } from 'react-router-dom';
 
 import { NewPost } from './components/NewPost';
 import { SkipPosting } from './components/SkipPosting';
-import { LandingMain } from '@pages/landing/Landing.style';
 import {
-  StyledPosting,
   ContentContainer,
-  StyledDescription
+  StyledDescription,
+  StyledPosting
 } from './Posting.style';
 
 const Posting = () => {
@@ -17,26 +16,26 @@ const Posting = () => {
   const customToken = `bearer ${token}`;
 
   return (
-    <LandingMain>
-      <StyledPosting>
-        <ContentContainer>
-          <StyledDescription>
-            <p>
-              총 <b>{totalTime / 60}</b>분동안 명상을 진행했어요!
-            </p>
-            <p>{channelLabel}에 대해 어떤 생각을 하셨나요?</p>
-          </StyledDescription>
-          <NewPost
-            channelId={channelId}
-            customToken={customToken}
-          />
-          <SkipPosting
-            channelId={channelId}
-            customToken={customToken}
-          />
-        </ContentContainer>
-      </StyledPosting>
-    </LandingMain>
+    <StyledPosting>
+      <ContentContainer>
+        <StyledDescription>
+          <p>
+            총 <b>{totalTime / 60}</b>분 동안 명상을 진행했어요!
+          </p>
+          <p>
+            <u>{channelLabel}</u>에 대해 어떤 생각을 하셨나요?
+          </p>
+        </StyledDescription>
+        <NewPost
+          channelId={channelId}
+          customToken={customToken}
+        />
+        <SkipPosting
+          channelId={channelId}
+          customToken={customToken}
+        />
+      </ContentContainer>
+    </StyledPosting>
   );
 };
 
