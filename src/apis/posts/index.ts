8ce958189d62a6154fe812a6638b7c376001837c
hyesung99 <4538asd@gmail.com>
2023-09-20 18:02:03 +0900

    ✨Feat : posts API 요청 추가

--- src/apis/posts/index.ts
+++ src/apis/posts/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@/constants/Api';
+import { Post, Channel } from '@/types';
 
-export const getPosts = async (
+const getPosts = async (
   channelId: string,
   offset: number = 0,
   limit: number = 10
@@ -18,4 +19,69 @@ export const getPosts = async (
   } catch (error) {
     console.log(error);
   }
+};
+
+const getChannels = async (channelName: string) => {
+  const response = await axios.get<Channel>(
+    `${API_BASE_URL}/channel/${channelName}`
+  );
+  return response.data;
+};
+
+const getSpecificUserPosts = async (
+  authorId: string,
+  offset?: number,
+  limit?: number
+) => {
+  const url = `${API_BASE_URL}/posts/author/${authorId}`;
+  const params = new URLSearchParams({
+    offset: `${offset}`,
+    limit: `${limit}`
+  });
+  const response = await axios.get<Post[]>(`${url}?${params}`);
+  return response.data;
+};
+
+const postPost = async (postData: FormData, token: string) => {
+  const response = await axios.post<Post>(
+    `${API_BASE_URL}/posts/create`,
+    postData,
+    {
+      headers: {
+        Authorization: token
+      }
+    }
+  );
+  return response.data;
+};
+
+const putPost = async (postData: FormData, token: string) => {
+  const response = await axios.put<Post>(
+    `${API_BASE_URL}/posts/update`,
+    postData,
+    {
+      headers: {
+        Authorization: token
+      }
+    }
+  );
+  return response.data;
+};
+
+const deletePost = async (postId: string, token: string) => {
+  const response = await axios.delete<Post>(`${API_BASE_URL}/posts/delete`, {
+    data: { id: postId },
+    headers: {
+      Authorization: token
+    }
+  });
+  return response.data;
+};
+export {
+  getChannels,
+  getPosts,
+  getSpecificUserPosts,
+  postPost,
+  putPost,
+  deletePost
 };
