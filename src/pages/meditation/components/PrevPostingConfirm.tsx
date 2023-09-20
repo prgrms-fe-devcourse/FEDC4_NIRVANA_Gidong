b210e409bbb21da45acaa5637cf0c7fb8f0dfafc
Suyeon <msjgr2801@gmail.com>
2023-09-20 16:50:43 +0900

    ✨ Feat: Confirm UI 구현

---
+++ src/pages/meditation/components/PrevPostingConfirm.tsx
@@ -0,0 +1,61 @@
+import { Confirm } from '@components/Confirm';
+import { Button } from '@components/Button';
+
+interface PrevPostingConfirmProps {
+  handleConfirmButton?: () => void;
+  handleCancelButton?: () => void;
+}
+
+const PrevPostingConfirm = ({
+  handleConfirmButton,
+  handleCancelButton
+}: PrevPostingConfirmProps) => {
+  const linkState = {
+    validate: true
+  };
+
+  const ConfirmButton = () => {
+    return (
+      <Button
+        width={120}
+        height={50}
+        label='이동'
+        dark={true}
+        bold={true}
+        handleClick={handleConfirmButton}
+      />
+    );
+  };
+
+  const CancelButton = () => {
+    return (
+      <Button
+        width={120}
+        height={50}
+        label='취소'
+        dark={false}
+        bold={true}
+        handleClick={handleCancelButton}
+      />
+    );
+  };
+
+  return (
+    <>
+      <Confirm
+        emoji={'✏️'}
+        content={`
+        작성하던 포스트가 있어요.
+        포스팅 화면으로 이동할까요?
+      `}
+        ConfirmButton={ConfirmButton}
+        CancelButton={CancelButton}
+        nextPageLink='/posting'
+        linkState={linkState}
+      />
+      ;
+    </>
+  );
+};
+
+export default PrevPostingConfirm;
--- src/pages/meditation/components/index.ts
+++ src/pages/meditation/components/index.ts
@@ -2,8 +2,10 @@ import MeditationEndButton from './MeditationEndButton';
 import MeditationLabel from './MeditationLabel';
 import MeditationTimer from './MeditationTimer';
 import MeditationTimeSetter from './MeditationTimeSetter';
+import PrevPostingConfirm from './PrevPostingConfirm';
 
 export {
+  PrevPostingConfirm,
   MeditationEndButton,
   MeditationLabel,
   MeditationTimer,
