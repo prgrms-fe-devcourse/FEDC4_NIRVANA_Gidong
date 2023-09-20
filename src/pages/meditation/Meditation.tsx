9f1a824ab0dbe3e6d1ba11509f089c8bfbfa6a95
Suyeon <msjgr2801@gmail.com>
2023-09-20 18:07:38 +0900

    🔨 Refactoring: 명상 중지 Confirm 컴포넌트 분리

--- src/pages/meditation/Meditation.tsx
+++ src/pages/meditation/Meditation.tsx
@@ -4,14 +4,13 @@ import { useState } from 'react';
 import { endButtonPushed } from './states';
 import { MeditationPage } from './Meditation.style';
 import { ThemePicker } from '@components/ThemePicker';
-import { Confirm } from '@components/Confirm';
-import { Button } from '@components/Button';
 import { meditationChannelInfo } from './models/channelInfo';
 import {
   PrevPostingConfirm,
   MeditationLabel,
   MeditationTimer,
-  MeditationTimeSetter
+  MeditationTimeSetter,
+  MeditationCancelConfirm
 } from '@pages/meditation/components';
 
 const Meditation = () => {
@@ -22,6 +21,10 @@ const Meditation = () => {
   const handleCancelButton = () => {
     sessionStorage.removeItem('posting');
     setHasPrevPosting(false);
+  };
+
+  const handleCancelCapture = () => {
+    setConfirmCaptured(false);
   };
 
   return (
@@ -35,31 +38,7 @@ const Meditation = () => {
         <MeditationTimeSetter />
         <ThemePicker themeInfo={meditationChannelInfo} />
         {confirmCaptured && (
-          <Confirm
-            emoji={'🧘🏻'}
-            content={'정말 명상을 끝내시겠어요?'}
-            contentFontSize={18}
-            nextPageLink={'/'}
-            CancelButton={
-              <Button
-                width={120}
-                height={50}
-                bold={true}
-                dark={false}
-                label={'취소'}
-                handleClick={() => setConfirmCaptured(false)}
-              />
-            }
-            ConfirmButton={
-              <Button
-                width={120}
-                height={50}
-                bold={true}
-                dark={true}
-                label={'끝내기'}
-              />
-            }
-          />
+          <MeditationCancelConfirm handleCancelButton={handleCancelCapture} />
         )}
       </MeditationPage>
     </>
--- src/pages/meditation/components/index.ts
+++ src/pages/meditation/components/index.ts
@@ -3,8 +3,10 @@ import MeditationLabel from './MeditationLabel';
 import MeditationTimer from './MeditationTimer';
 import MeditationTimeSetter from './MeditationTimeSetter';
 import PrevPostingConfirm from './PrevPostingConfirm';
+import MeditationCancelConfirm from './MeditationCancelConfirm';
 
 export {
+  MeditationCancelConfirm,
   PrevPostingConfirm,
   MeditationEndButton,
   MeditationLabel,
