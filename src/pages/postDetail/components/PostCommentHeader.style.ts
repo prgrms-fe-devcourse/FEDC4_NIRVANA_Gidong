6b00e4d484930790c64cbfb7402543dad013283b
hyesung99 <4538asd@gmail.com>
2023-09-21 19:02:27 +0900

    ✨Feat : PostCommentHeader 스타일 추가

--- src/pages/postDetail/components/PostCommentHeader.style.ts
+++ src/pages/postDetail/components/PostCommentHeader.style.ts
@@ -7,13 +7,45 @@ export const PostCommentHeaderContainer = styled.header`
   padding-bottom: 10px;
   width: 100%;
   height: 40px;
-  > span {
-    margin-top: 2px;
-  }
 `;
 
 export const PostCommentHeaderText = styled.p`
   margin: 0 5px 0 3px;
   font-size: 14px;
   font-weight: 800;
+`;
+
+export const PostLikeContainer = styled.div`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  user-select: none;
+  > span {
+    transition: transform 0.2s ease-in-out;
+    margin-top: 1px;
+    &:active {
+      animation: shake 1s infinite;
+    }
+  }
+
+  @keyframes shake {
+    0% {
+      transform: rotate(0) scale(1.2);
+    }
+    10% {
+      transform: rotate(15deg) scale(1.2);
+    }
+    30% {
+      transform: rotate(-15deg) scale(1.2);
+    }
+    50% {
+      transform: rotate(15deg) scale(1.2);
+    }
+    70% {
+      transform: rotate(0) scale(1.2);
+    }
+    100% {
+      transform: rotate(0) scale(1.2);
+    }
+  }
 `;
