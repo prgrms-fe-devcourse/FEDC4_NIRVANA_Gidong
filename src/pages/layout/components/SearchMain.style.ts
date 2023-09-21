972c3f3527a09d44e13600a025f731cdafecaab5
Park Nayeon <within197@naver.com>
2023-09-21 21:56:03 +0900

    💄 Style:	SearchItem 스타일 추가

--- src/pages/layout/components/SearchMain.style.ts
+++ src/pages/layout/components/SearchMain.style.ts
@@ -14,3 +14,9 @@ export const SearchResult = styled.div`
   height: calc(100vh - 50px - 64px - 61px);
   overflow: auto;
 `;
+
+export const SearchItem = styled.div`
+  ${({ theme }) => theme.style.flexAlignCenter}
+  padding : 0 26px;
+  height: 80px;
+`;
