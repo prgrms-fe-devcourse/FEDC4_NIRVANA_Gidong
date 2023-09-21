1a76d9864489979440d5525c471390140a51429d
Cheshier <chungup5495@gmail.com>
2023-09-21 14:03:18 +0900

    🔨 Refactor: themeProvider 타입 추론 가능

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,12 +1,20 @@
 import '@emotion/react';
+import { color } from '@styles/colors';
+import { style } from '@styles/styles';
+
+type color = typeof color;
+type style = typeof style;
+
+type c = keyof color;
+type s = keyof style;
 
 declare module '@emotion/react' {
-  export interface Theme {
+  export interface Theme extends color, style {
     color: {
-      [key: string]: string;
+      [key in c]: string;
     };
     style: {
-      [key: string]: string;
+      [key in s]: string;
     };
   }
 }
