db2c4189898d0bd942116b5ed8f00ea084f2bd5a
Park Nayeon <within197@naver.com>
2023-09-21 16:39:12 +0900

    ✨ Feat:	Search API 요청 타입 추가

--- src/apis/search/index.ts
+++ src/apis/search/index.ts
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@constants/Api';
+import { User, Post } from '@/types';
 
 const searchUser = async (query: string) => {
-  const response = await axios.get(`${API_BASE_URL}/search/users/${query}`);
+  const response = await axios.get<User[]>(
+    `${API_BASE_URL}/search/users/${query}`
+  );
 
   return response.data;
 };
 
 const searchPost = async (query: string) => {
-  const response = await axios.get(`${API_BASE_URL}/search/all/${query}`);
+  const response = await axios.get<(User | Post)[]>(
+    `${API_BASE_URL}/search/all/${query}`
+  );
 
   return response.data;
 };
