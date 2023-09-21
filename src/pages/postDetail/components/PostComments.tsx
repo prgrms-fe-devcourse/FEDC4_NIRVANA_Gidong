0af539ebe5d790fc03b20ae01c8c8682e3e145b4
hyesung99 <4538asd@gmail.com>
2023-09-21 19:08:21 +0900

    ✨Feat : 댓글 달았을때 refetch

--- src/pages/postDetail/components/PostComments.tsx
+++ src/pages/postDetail/components/PostComments.tsx
@@ -3,21 +3,43 @@ import { PostCommentHeader, PostComment } from './';
 import { Comment } from '@/types/Comment';
 
 interface PostCommentsProps {
+  postId: string;
+  token: string;
+  currentUserId: string;
   comments: Comment[];
+  refetch: () => void;
+  likedThisPost: boolean;
+  likeCounts: number;
 }
 
-const PostComments = ({ comments }: PostCommentsProps) => {
+const PostComments = ({
+  postId,
+  token,
+  currentUserId,
+  refetch,
+  comments,
+  likedThisPost,
+  likeCounts
+}: PostCommentsProps) => {
   return (
     <PostCommentsSection>
       <PostCommentHeader
-        postLikeCount={10}
-        postCommentCount={20}
+        postId={postId}
+        token={token}
+        refetch={refetch}
+        likedThisPost={likedThisPost}
+        likeCounts={likeCounts}
+        postCommentCount={comments?.length}
       />
       {comments?.map((comment: Comment) => (
         <PostComment
           key={comment._id}
+          id={comment._id}
+          token={token}
           author={comment.author}
           text={comment.comment}
+          myComment={comment.author._id === currentUserId}
+          refetch={refetch}
         />
       ))}
     </PostCommentsSection>
