d70e7ce4eee8c74ec9ce62c009b957bea81616e3
Park Nayeon <within197@naver.com>
2023-09-22 04:17:21 +0900

    ✨ Feat:	헤더에 알림 목록 가져오기 추가

--- src/pages/layout/components/AlertButton.tsx
+++ src/pages/layout/components/AlertButton.tsx
@@ -1,16 +1,47 @@
+import { useLocation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getNotifications } from '@apis/notice';
+import useSessionStorage from '@hooks/useSessionStorage';
 import { Link } from '@components/Link';
 import { Icon } from '@components/Icon';
 import { DotBadge } from '@components/Badge';
+import { User } from '@/types';
 
 const AlertButton = () => {
+  const [userSessionData] = useSessionStorage<Pick<User, '_id' | 'token'>>(
+    'userData',
+    {
+      _id: '',
+      token: ''
+    }
+  );
+
+  const location = useLocation();
+  const { pathname } = location;
+  const { token } = userSessionData;
+
+  const query = useQuery({
+    queryKey: ['headerAlert'],
+    queryFn: async () => {
+      const data = await getNotifications(`Bearer ${token}`);
+      return data;
+    },
+    refetchInterval: () => (pathname === '/notice' ? false : 5000),
+    refetchIntervalInBackground: true
+  });
+
+  const { data } = query;
+
+  console.log(data);
+
   return (
     <DotBadge
-      dot={true}
+      dot={Boolean(data?.length > 0)}
       color='orange'
       position='top'
       badgeSize={5}>
       <Link
-        pageLink='/alert'
+        pageLink='/notice'
         size={23}>
         <Icon
           name='notifications'
