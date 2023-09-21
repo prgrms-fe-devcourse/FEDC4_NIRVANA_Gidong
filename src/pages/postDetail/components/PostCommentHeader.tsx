2cc1d59ee4b9107136701357f97526bc26ec57da
hyesung99 <4538asd@gmail.com>
2023-09-21 19:01:53 +0900

    ✨Feat : likedThisPost prop으로 받아서 렌더링

--- src/pages/postDetail/components/PostCommentHeader.tsx
+++ src/pages/postDetail/components/PostCommentHeader.tsx
@@ -1,26 +1,50 @@
 import { Icon } from '@components/Icon';
+import { useMutation } from '@tanstack/react-query';
+import { postLike, deleteLike } from '@apis/Like';
 import {
   PostCommentHeaderContainer,
-  PostCommentHeaderText
+  PostCommentHeaderText,
+  PostLikeContainer
 } from './PostCommentHeader.style';
 
 interface PostCommentHeaderProps {
-  postLikeCount: number;
+  postId: string;
+  token: string;
+  likeCounts: number;
+  likedThisPost: boolean;
+  refetch: () => void;
   postCommentCount: number;
 }
 
 const PostCommentHeader = ({
-  postLikeCount = 0,
+  postId,
+  token,
+  refetch,
+  likedThisPost,
+  likeCounts,
   postCommentCount = 0
 }: PostCommentHeaderProps) => {
+  const { mutate, isSuccess } = useMutation(() => {
+    return likedThisPost ? deleteLike(postId, token) : postLike(postId, token);
+  });
+
+  if (isSuccess) {
+    refetch();
+  }
+  const handleLikeClick = () => {
+    mutate();
+  };
+
   return (
     <PostCommentHeaderContainer>
-      <Icon
-        name='favorite'
-        color='purpleVivid'
-        fill={true}
-      />
-      <PostCommentHeaderText>{postLikeCount}</PostCommentHeaderText>
+      <PostLikeContainer onClick={handleLikeClick}>
+        <Icon
+          name='favorite'
+          color='purpleVivid'
+          fill={likedThisPost}
+        />
+      </PostLikeContainer>
+      <PostCommentHeaderText>{likeCounts}</PostCommentHeaderText>
       <PostCommentHeaderText>댓글 {postCommentCount}개</PostCommentHeaderText>
     </PostCommentHeaderContainer>
   );
