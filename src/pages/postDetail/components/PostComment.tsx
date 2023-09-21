504372b3f5f1c53374638f0f1d972f2987ef1fd7
hyesung99 <4538asd@gmail.com>
2023-09-21 19:09:34 +0900

    ✨Feat : 내 댓글 판단해서 삭제 댓글 삭제 기능 추가

--- src/pages/postDetail/components/PostComment.tsx
+++ src/pages/postDetail/components/PostComment.tsx
@@ -3,34 +3,63 @@ import {
   PostCommentContainer,
   PostCommentAvatarContainer,
   PostCommentUserContainer,
-  PostCommentContentContainer
+  PostCommentContentContainer,
+  PostCommentDeleteContainer
 } from './PostComment.style';
 import { UserId, UserName } from '@components/UserText';
 import { User } from '@/types/User';
+import { useMutation } from '@tanstack/react-query';
+import { deleteComment } from '@apis/comment';
 
 interface PostCommentProps {
   author: User;
   text: string;
+  token: string;
+  myComment: boolean;
+  id: string;
+  refetch: () => void;
 }
 
-const PostComment = ({ author, text }: PostCommentProps) => {
+const PostComment = ({
+  author,
+  text,
+  token,
+  myComment,
+  id,
+  refetch
+}: PostCommentProps) => {
+  const { mutate, isSuccess } = useMutation(deleteComment);
+
+  if (isSuccess) refetch();
+
+  const handleCommentDeleteClick = async () => {
+    mutate({ id, token });
+  };
+
   return (
-    <PostCommentContainer>
-      <PostCommentAvatarContainer>
-        <Avatar
-          size={39}
-          src={author.image ? author.image : ''}
-          alt={author.fullName}
-        />
-      </PostCommentAvatarContainer>
-      <PostCommentContentContainer>
-        <PostCommentUserContainer>
-          <UserName>{author.fullName}</UserName>
-          <UserId email={author.email} />
-        </PostCommentUserContainer>
-        {text}
-      </PostCommentContentContainer>
-    </PostCommentContainer>
+    <>
+      <PostCommentContainer>
+        <PostCommentAvatarContainer>
+          <Avatar
+            size={39}
+            src={author.image ? author.image : ''}
+            alt={author.fullName}
+          />
+        </PostCommentAvatarContainer>
+        <PostCommentContentContainer>
+          <PostCommentUserContainer>
+            <UserName>{author.fullName}</UserName>
+            <UserId email={author.email} />
+          </PostCommentUserContainer>
+          {text}
+        </PostCommentContentContainer>
+      </PostCommentContainer>
+      {myComment && (
+        <PostCommentDeleteContainer onClick={handleCommentDeleteClick}>
+          삭제
+        </PostCommentDeleteContainer>
+      )}
+    </>
   );
 };
 
