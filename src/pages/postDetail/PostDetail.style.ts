6c9ab2c2c332d2705217fef478f34baf8acc42d4
hyesung99 <4538asd@gmail.com>
2023-09-22 15:05:16 +0900

    💄 Style:	overflow 속성 변경

--- src/pages/postDetail/PostDetail.style.ts
+++ src/pages/postDetail/PostDetail.style.ts
@@ -4,7 +4,7 @@ export const PostDetailPage = styled.div`
   ${({ theme }) => theme.style.flexAlignCenter};
   padding: 30px 20px;
   flex-direction: column;
-  overflow: scroll;
+  overflow-y: auto;
   width: 100%;
   height: 100%;
 `;
