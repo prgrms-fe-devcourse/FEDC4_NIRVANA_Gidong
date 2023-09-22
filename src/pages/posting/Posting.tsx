7545008e8113fe76da811a5be988893ed7bd110a
Suyeon <msjgr2801@gmail.com>
2023-09-22 17:02:14 +0900

    ✨ Feat: Posting 임의 접근시 404 페이지로 이동

--- src/pages/posting/Posting.tsx
+++ src/pages/posting/Posting.tsx
@@ -1,4 +1,5 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { NewPost } from './components/NewPost';
 import { SkipPosting } from './components/SkipPosting';
@@ -7,24 +8,38 @@ import {
   StyledDescription,
   StyledPosting
 } from './Posting.style';
+import PostingHelper from './components/NewPost/PostingHelper';
+
+interface ReceiveState {
+  totalTime: number;
+  channelId: string;
+  channelLabel: string;
+}
 
 const Posting = () => {
   const location = useLocation();
-  const { channelId, channelLabel, totalTime } = location.state;
-
+  const navigate = useNavigate();
   const { token } = JSON.parse(sessionStorage.getItem('userData'));
+  const [meditationInfo, setMeditationInfo] = useState<ReceiveState>();
+  const { totalTime, channelLabel, channelId } = meditationInfo;
   const customToken = `bearer ${token}`;
+
+  useEffect(() => {
+    if (location.state === null) {
+      navigate('/404');
+    }
+
+    setMeditationInfo(location.state);
+  }, []);
 
   return (
     <StyledPosting>
       <ContentContainer>
         <StyledDescription>
-          <p>
-            총 <b>{totalTime / 60}</b>분 동안 명상을 진행했어요!
-          </p>
-          <p>
-            <u>{channelLabel}</u>에 대해 어떤 생각을 하셨나요?
-          </p>
+          <PostingHelper
+            totalTime={totalTime}
+            channelLabel={channelLabel}
+          />
         </StyledDescription>
         <NewPost
           channelId={channelId}
