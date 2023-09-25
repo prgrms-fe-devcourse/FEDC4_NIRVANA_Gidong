91cc5ae6449fdbfae0aa3cfdc387966d9731eeb0
Suyeon <msjgr2801@gmail.com>
2023-09-25 22:36:47 +0900

    🔨 Refactoring: url, 반환값 분리

--- src/apis/posting/index.ts
+++ src/apis/posting/index.ts
@@ -3,11 +3,14 @@ import { API_BASE_URL } from '@/constants/Api';
 
 const postCreateNewPost = async (token: string, formData: FormData) => {
   try {
-    return await axios.post(`${API_BASE_URL}/posts/create`, formData, {
+    const url = `${API_BASE_URL}/posts/create`;
+    const { data } = await axios.post(url, formData, {
       headers: {
         Authorization: token
       }
     });
+
+    return data;
   } catch (error) {
     throw new Error(error);
   }
