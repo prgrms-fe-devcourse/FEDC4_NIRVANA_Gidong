dc6a9f071c283dfa3c5d5cea4a15ee25bad81311
Park Nayeon <within197@naver.com>
2023-09-25 21:38:46 +0900

    ✨ Feat:	login 확인 모달 생성

---
+++ src/components/Confirm/LoginConfirm.tsx
@@ -0,0 +1,45 @@
+import Confirm from './Confirm';
+import { Button } from '@components/Button';
+
+interface LoginConfirm {
+  handleClickCancel: () => void;
+  handleClickConfirm: () => void;
+  location: string;
+}
+
+const LoginConfirm = ({
+  handleClickCancel,
+  handleClickConfirm,
+  location
+}: LoginConfirm) => {
+  return (
+    <Confirm
+      emoji='🔒'
+      content={`로그인이 필요한 서비스입니다.
+       로그인하시겠습니까?`}
+      contentFontSize={16}
+      nextPageLink={`/login?redirect=${location}`}
+      CancelButton={
+        <Button
+          width={120}
+          height={50}
+          bold={true}
+          dark={false}
+          label={'취소'}
+          handleClick={handleClickCancel}
+        />
+      }
+      ConfirmButton={
+        <Button
+          width={120}
+          height={50}
+          bold={true}
+          dark={true}
+          label={'확인'}
+          handleClick={handleClickConfirm}
+        />
+      }
+    />
+  );
+};
+export default LoginConfirm;
--- src/pages/layout/components/IconNav.tsx
+++ src/pages/layout/components/IconNav.tsx
@@ -1,29 +1,61 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AlertButton, SearchButton } from '@pages/layout/components';
 import { Icon } from '@components/Icon';
 import { Link } from '@components/Link';
-import { AlertButton, SearchButton } from '@pages/layout/components';
+import { LoginConfirm } from '@components/Confirm';
+import useSessionStorage from '@hooks/useSessionStorage';
 import { EtcNavContainer } from './IconNav.style';
-
+import { User } from '@/types';
 interface EtcNavProps {
   handleShowSearchBox: () => void;
   showSearchBox: boolean;
 }
 
 const EtcNav = ({ handleShowSearchBox, showSearchBox }: EtcNavProps) => {
+  const [modal, setModal] = useState(false);
+  const [{ _id, token }] = useSessionStorage<Pick<User, '_id' | 'token'>>(
+    'userData',
+    {
+      _id: '',
+      token: ''
+    }
+  );
+  const navigate = useNavigate();
+
+  const handleShowModal = () => {
+    setModal((prev) => !prev);
+  };
+
+  const handleClickAlert = () => {
+    if (_id && token) navigate('/notice');
+    else setModal((prev) => !prev);
+  };
+
   return (
-    <EtcNavContainer>
-      <SearchButton
-        handleClickButton={handleShowSearchBox}
-        searchStatus={showSearchBox}
-      />
-      <AlertButton />
-      <Link pageLink='/message'>
-        <Icon
-          name='chat'
-          color='white'
-          size={23}
+    <>
+      {modal && (
+        <LoginConfirm
+          handleClickCancel={handleShowModal}
+          handleClickConfirm={handleShowModal}
+          location={'notice'}
         />
-      </Link>
-    </EtcNavContainer>
+      )}
+      <EtcNavContainer>
+        <SearchButton
+          handleClickButton={handleShowSearchBox}
+          searchStatus={showSearchBox}
+        />
+        <AlertButton handleClickAlert={handleClickAlert} />
+        <Link pageLink='/message'>
+          <Icon
+            name='chat'
+            color='white'
+            size={23}
+          />
+        </Link>
+      </EtcNavContainer>
+    </>
   );
 };
 
