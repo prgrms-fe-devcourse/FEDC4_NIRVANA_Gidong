f706c5067af831dec84071fcb056470b9dbef1e5
Suyeon <msjgr2801@gmail.com>
2023-09-25 15:38:13 +0900

    💄 Style: height style 수정

--- src/pages/posting/components/SkipPosting/SkipPosting.style.ts
+++ src/pages/posting/components/SkipPosting/SkipPosting.style.ts
@@ -3,8 +3,8 @@ import styled from '@emotion/styled';
 export const StyledSkipPosting = styled.div`
   cursor: pointer;
   ${({ theme }) => theme.style.flexCenter};
-  height: 50px;
+  width: 100%;
+  min-height: 40px;
   color: ${({ theme }) => theme.color.white};
   font-size: 14px;
-  padding-top: 20px;
 `;
