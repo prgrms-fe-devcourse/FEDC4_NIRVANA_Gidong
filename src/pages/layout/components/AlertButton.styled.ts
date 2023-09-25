4eeca4dcc1e51c8c2136de1a5e7f5ed79b3f7030
Suyeon <msjgr2801@gmail.com>
2023-09-26 02:17:30 +0900

    💄 Style: header button hover 효과 주기

--- src/pages/layout/components/AlertButton.styled.ts
+++ src/pages/layout/components/AlertButton.styled.ts
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 
 export const AlertContainer = styled.div`
   width: 40px;
-  height: 100%;
+  height: 40px;
+  border-radius: 50%;
   ${({ theme }) => theme.style.flexCenter};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.black250};
+  }
 `;
--- src/pages/layout/components/SearchButton.style.ts
+++ src/pages/layout/components/SearchButton.style.ts
@@ -2,11 +2,16 @@ import styled from '@emotion/styled';
 
 export const ButtonBox = styled.div`
   width: 40px;
-  height: 100%;
+  height: 40px;
+  border-radius: 50%;
   ${({ theme }) => theme.style.flexCenter}
   & > button {
     border: none;
     background: none;
     padding: 0;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.black250};
   }
 `;
