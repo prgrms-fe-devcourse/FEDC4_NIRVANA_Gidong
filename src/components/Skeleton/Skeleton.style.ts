a9e761f8c18b8ae9c59d27518f0ef4e1a5d7caa4
hyesung99 <4538asd@gmail.com>
2023-09-27 14:26:33 +0900

    ✨Feat : 공통 스켈레톤 UI 파일 옮김

--- src/components/Skeleton/Skeleton.style.ts
+++ src/components/Skeleton/Skeleton.style.ts
@@ -20,11 +20,52 @@ const StyledSkeleton = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    width: 60px;
+    width: 100%;
     height: 100%;
     background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2);
     animation: loading 2s infinite linear;
   }
 `;
 
-export { StyledSkeleton };
+const SkeletonHeader = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
+
+const SkeletonAvatar = styled(StyledSkeleton)`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const SkeletonUser = styled(StyledSkeleton)`
+  width: calc(100% - 70px);
+  margin-left: 20px;
+  height: 40px;
+  padding-left: 10px;
+  border-radius: 10px;
+`;
+
+const SkeletonContent = styled(StyledSkeleton)`
+  margin-top: 10px;
+  width: 80%;
+  height: 20px;
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
+
+const SkeletonLongRectangle = styled(StyledSkeleton)`
+  width: 100%;
+  height: 80px;
+`;
+
+export {
+  StyledSkeleton,
+  SkeletonHeader,
+  SkeletonAvatar,
+  SkeletonUser,
+  SkeletonContent,
+  SkeletonLongRectangle
+};
