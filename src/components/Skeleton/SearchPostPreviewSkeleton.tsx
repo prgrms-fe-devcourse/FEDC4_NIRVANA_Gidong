a37af78eb23bd549d336757240170956ed522da1
Park Nayeon <within197@naver.com>
2023-09-27 10:43:50 +0900

    ✨ Feat:	스켈레톤 UI 커스텀

---
+++ src/components/Skeleton/SearchPostPreviewSkeleton.tsx
@@ -0,0 +1,14 @@
+import PostPreviewSkeleton from './PostPreviewSkeleton';
+
+const SearchPostPreviewSkeleton = () => {
+  return (
+    <>
+      <PostPreviewSkeleton />
+      <PostPreviewSkeleton />
+      <PostPreviewSkeleton />
+      <PostPreviewSkeleton />
+    </>
+  );
+};
+
+export default SearchPostPreviewSkeleton;
