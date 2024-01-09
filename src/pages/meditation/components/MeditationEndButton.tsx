7008cb08a2fae8cd9a1b5315d52ba514af910f7e
sscoderati <chungup5495@gmail.com>
2024-01-09 16:32:56 +0900

    Fix: 명상 종료 버튼 핸들러에 명상 관련 정보 초기화 로직 추가

--- src/pages/meditation/components/MeditationEndButton.tsx
+++ src/pages/meditation/components/MeditationEndButton.tsx
@@ -2,16 +2,28 @@ import { useState } from 'react';
 import { Confirm } from '@components/Confirm';
 import { Button } from '@components/Button';
 import { EndButtonContainer } from './MeditationEndButton.style';
+import { MeditationStatusType } from '@pages/meditation/types';
+import { intervalId, meditationTime } from '@pages/meditation/states';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
-const MeditationEndButton = () => {
+const MeditationEndButton = ({
+  statusSetter: meditationStatusSetter
+}: {
+  statusSetter: React.Dispatch<React.SetStateAction<MeditationStatusType>>;
+}) => {
   const [confirmCaptured, setConfirmCaptured] = useState(false);
+  const setTime = useSetRecoilState(meditationTime);
+  const [timerId, setTimerId] = useRecoilState(intervalId);
 
   const handleCancelButton = () => {
     setConfirmCaptured(false);
   };
 
   const handleConfirmButton = () => {
-    location.reload(); // 리팩토링 전 임시방편
+    meditationStatusSetter({ started: false, paused: true, ended: false });
+    clearInterval(timerId);
+    setTimerId(0);
+    setTime(0);
   };
   return (
     <>
